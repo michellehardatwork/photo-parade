864de3ca29398840d1f4cc1ffc810ce174f716a5
Michelle Cervenka <cervenka.michelle@heb.com>
2020-02-07 18:04:22 -0600

    add networking

--- PhotoParade/Photos/Services/PhotoService.swift
+++ PhotoParade/Photos/Services/PhotoService.swift
@@ -25,10 +25,40 @@ class PhotoService {
     }
     
     func search(_ request: PhotoRequest, completion: @escaping (Result<PhotoResult, Error>) -> Void) {
-        // TODO: get data
-        
-        DispatchQueue.main.async {
-            completion(Result.failure(PhotoServiceError.unexpectedHttpResponse))
+
+        guard let url = photoProvider.url(request) else {
+            completion(Result.failure(PhotoServiceError.malformedURL))
+            return
         }
+
+        //Make new request to get search results
+        dataTask = defaultSession.dataTask(with: url) { data, response, error in
+
+            //Checking for error
+            if let error = error {
+                print("Photo Service: Error while searching photos: \(error.localizedDescription)")
+                DispatchQueue.main.async {
+                    completion(Result.failure(error))
+                }
+                return
+            }
+
+            //Checking HTTP response
+            guard let response = response as? HTTPURLResponse,
+                response.statusCode == 200 else {
+                    print("Photo Service: HTTP error")
+                    DispatchQueue.main.async {
+                        completion(Result.failure(PhotoServiceError.unexpectedHttpResponse))
+                    }
+                    return
+            }
+
+            //Returning photos
+            DispatchQueue.main.async {
+                completion(Result.success(self.photoProvider.photos(data)))
+            }
+        }
+
+        dataTask?.resume()
     }
 }
